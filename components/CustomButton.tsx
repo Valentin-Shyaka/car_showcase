5b60a6942fdbde4dd8b49993fa1b89140a1d5567
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-09-25 07:21:13 +0200

    [fix]: fixed the prop-type issue in components...

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+ interface Props{
+  text:string;
+  active:boolean;
+ }
 
-
-const CustomButton = ({text,active}) => {
+const CustomButton = ({text,active}:Props) => {
     
   return (
     <button className={active ?'p-4 border bg-[#FF7A1A] rounded-lg font-bold text-white':'p-4 border border-[#FF7A1A] rounded-lg font-bold'}>
