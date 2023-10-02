6e0ab6f62788b8e90a5b2ee3f26d18b66a644fd8
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-10-02 20:55:57 +0200

    Contact page continus implementation...

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const HostPage = () => {
   return (
     <div className='p-10 bg-[#f2f4f8]'>
-        contactPage
+        <h3 className='text-2xl font-bold'>Talk to our team.</h3>
+        <div className='border w-full p-8 rounded-lg mt-4'>
+
+        </div>
     </div>
   )
 }
