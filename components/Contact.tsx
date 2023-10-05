a50aaf16bcd8e0fa3b49df1a1ca308d909563c37
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-10-05 11:21:30 +0200

    [fixed]: the event and input handled...

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,7 +6,7 @@ import CustomButton from './CustomButton';
 
 const Contact = () => {
     const form = useRef();
-    const sendEmail = (e) => {
+    const sendEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
        
         e.preventDefault();
     
