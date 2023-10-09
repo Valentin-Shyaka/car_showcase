551db720f8f0e6c428faf29d4ba02637f98577ab
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-10-09 11:30:43 +0200

    memberCard implementation...

--- app/ourTeam/page.tsx
+++ app/ourTeam/page.tsx
@@ -1,3 +1,4 @@
+import TeamMemberCard from '@/components/TeamMemberCard'
 import React from 'react'
 
 const TeamPage = () => {
@@ -7,12 +8,16 @@ const TeamPage = () => {
             <img
              src="https://images.unsplash.com/photo-1510227320292-0811fae44991?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80"
              alt=""
-             className='w-full h-[40vh] object-cover'
+             className='w-full h-[40vh] object-cover bg-scroll'
               />
               <div className='p-36 text-center w-full h-[40vh] bg-[#00000045] absolute top-0'>
-                <h3 className='text-6xl text-white font-extrabold'>Meet the team</h3>
+                <h3 className='text-6xl text-white font-extrabold font-serif'>Meet the team</h3>
                 <p className='mt-4 text-white text-lg font-bold'>Get to know the people behind the wheel of the world's largest car sharing marketplace</p>
               </div>
+        </div>
+        <div className='p-20'>
+            <h3 className='text-4xl font-extrabold font-serif'>Leadership</h3>
+            <TeamMemberCard/>
         </div>
     </div>
   )
