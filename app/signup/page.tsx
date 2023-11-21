ecce7eff3cd54d4519b8640bda000be318d2c393
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-11-21 10:56:31 +0200

    improving the signup page ...

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -8,17 +8,27 @@ const page = () => {
                   <h3 className="text-3xl font-extrabold ">SIGN  UP</h3>
                   <div className="mt-4 flex flex-col gap-4">
                     <div>
-                        <label htmlFor="">Country code</label>
+                        <label htmlFor="">Location /Country code </label>
                         <input type="text" name="code" id="" placeholder="+250 Rwanda" className="outline-none w-full h-12 indent-4 border border-slate-300 rounded-xl" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Email</label>
+                        <input type="text" name="code" id="" placeholder="example@company.com" className="outline-none w-full h-12 indent-4 border border-slate-300 rounded-xl" />
+                    </div>
+                    <div>
+                        <label htmlFor="">ID no.</label>
+                        <input type="text" name="code" id="" placeholder="xxxx-xxxx-xxxx-xxxx" className="outline-none w-full h-12 indent-4 border border-slate-300 rounded-xl" />
+                    </div>
+                    <div>
+                        <label htmlFor="">password</label>
+                        <input type="password" name="code" id="" placeholder="" className="outline-none w-full h-12 indent-4 border border-slate-300 rounded-xl" />
                     </div>
                     <div>
                         <label htmlFor="">Phone number</label>
                         <input type="text" name="code" id="" placeholder="phone number" className="outline-none w-full h-12 indent-4 border border-slate-300 rounded-xl" />
-                        <p className="text-slate-500 text-sm">We'll text you a code to confirm your number</p>
+                        <p className="text-slate-500 text-sm">We'll text you a code to confirm your account</p>
                     </div>
-                    <label className="text-sm flex gap-2 text-slate-700">
-                        <input type="checkbox" className="accent-[#FF7A1A]" /> Remember me
-                    </label>
+                   
                     <CustomButton btnType="submit" text="Login" active/>
                   </div>
                   <span className="text-center text-slate-500">or</span>
@@ -26,8 +36,8 @@ const page = () => {
                     <CustomButton btnType="submit" text="Continue with Email" active={false}/>
                     
                   </div>
-                  <p className="text-center mt-2">Don't have an account? <span className="font-semibold cursor-pointer">Signup</span></p>   
-                  <footer className="text-sm text-center font-semibold">By logging in, you agree to eTware Inc.'s <span className="text-sky-900 text-sm cursor-pointer">terms of service and policy</span></footer>
+                  <p className="text-center mt-2">Already have an account? <span className="font-semibold cursor-pointer">Signin</span></p>   
+                  <footer className="text-sm text-center font-semibold">By signing in, you agree to eTware Inc.'s <span className="text-sky-900 text-sm cursor-pointer">terms of service and policy</span></footer>
           </div>
           </section>
 
