9b02d0bd42850b9135c9b1732729715c2c7e96ae
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-11-27 21:26:26 +0200

    landing page testing...

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,7 +1,8 @@
+
 describe('Navigation', () => {
   it('should navigate to the about page', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
  
     // Find a link with an href attribute containing "about" and click it
     cy.get('a[href*="home"]').click()
@@ -10,6 +11,20 @@ describe('Navigation', () => {
     cy.url().should('include', '/home')
  
     // The new page should contain an h1 with "About page"
-    cy.get('h1').contains('')
+    cy.get('h3').contains('200+ cars available')
+  }),
+  it('should navigate to the team page', () => {
+    // Start from the index page
+    cy.visit('/')
+ 
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="ourTeam"]').click()
+ 
+    // The new url should include "/about"
+    cy.url().should('include', '/ourTeam')
+ 
+    // The new page should contain an h1 with "About page"
+    cy.get('h3').contains('Meet the team')
   })
+  
 })
