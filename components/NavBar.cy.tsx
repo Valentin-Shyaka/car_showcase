ab4e7b10efc51c9f4c1d79b9ae5f4433d9a4a2dd
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-11-28 09:14:41 +0200

    tested the login renderer...

--- components/NavBar.cy.tsx
+++ components/NavBar.cy.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Login from './Login'
 
 describe('<NavBar />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<NavBar />)
-    cy.get
+    cy.mount(<Login isOpen={true} closeModal={()=>{}}/>)
+    
+  
   })
 })
